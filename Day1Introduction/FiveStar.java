52b5152f30f378581936a6f1ef3480043fc73125
deepzsenu <deepzsenu@gmail.com>
2022-11-07 22:18:17 +0530

    created  repo

---
+++ Day1Introduction/FiveStar.java
@@ -0,0 +1,35 @@
+/**
+Five Stars
+In this task, you have to print 5 stars ("*") vertically and 5 horizontally.
+
+Input Format:
+There is no input for the program.
+
+Output Format:
+* * * * *
+*
+*
+*
+*
+*
+Explanation:
+
+Print 5 horizontal stars separated by 1 space followed by 5 vertical stars.
+
+Common Mistakes
+You do not need for loop here.
+ */
+
+public class FiveStar {
+    public static void main(String args[]) {
+        // your code here
+      for (int i = 0; i<5; i++){
+        System.out.print("* ");
+      }
+      System.out.println();
+      for (int i = 0; i<5; i++){
+        System.out.println("*");
+      }
+        
+    }
+}
---
+++ Day1Introduction/check.java
@@ -0,0 +1,39 @@
+/*Welcome To Java
+Welcome to the world of Java!
+
+In this challenge, you will learn how to print “Hello World!” in standard output (stdout).
+
+Complete the code by copying the two lines of code below and pasting them inside the body of your main method.
+
+System.out.println("Hello, World.");
+System.out.println("Hello, Java.");
+Input Format
+There is no input for this challenge.
+
+Output Format
+You must print two lines of output:
+
+Print "Hello, World." on the first line (without quotes).
+
+Print "Hello, Java." on the second line (without quotes).
+
+Sample Output
+Hello, World.
+Hello, Java.
+Common Mistakes -
+Make sure you match the exact output required in question, taking care of commas, spaces and full stop exactly.
+Make sure you use S(capital) in System
+
+ */
+
+
+
+public class check
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+    System.out.println("Hello, World.");
+    System.out.println("Hello, Java.");
+	}
+}
