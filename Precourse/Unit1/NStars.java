f6057435e80307bcf9a597b105c6bb7fbcd59057
deepzsenu <deepzsenu@gmail.com>
2022-11-16 19:28:08 +0530

    Unit1

---
+++ Precourse/Unit1/NStars.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String args[]) {
+        // your code here
+      for (int i = 0; i<5; i++){
+        System.out.print("* ");
+      }
+      System.out.println();
+      for (int i = 0; i<5; i++){
+        System.out.println("*");
+      }
+    }
+}
---
+++ Precourse/Unit1/Welcome.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+    System.out.println("Hello, World.");
+System.out.println("Hello, Java.");
+	}
+}
