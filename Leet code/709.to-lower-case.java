e76840a95576d5ee458339050bf81fbe98202140
deepzsenu <deepzsenu@gmail.com>
2023-01-14 16:49:25 +0530

    leetcode

---
+++ Leet code/709.to-lower-case.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=709 lang=java
+ *
+ * [709] To Lower Case
+ */
+
+// @lc code=start
+class Solution {
+    public String toLowerCase(String s) {
+        String str  = s.toLowerCase();
+        return str;
+    }
+}
+// @lc code=end
+
---
+++ Leet code/953.verifying-an-alien-dictionary.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=953 lang=java
+ *
+ * [953] Verifying an Alien Dictionary
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        
+    }
+}
+// @lc code=end
+
